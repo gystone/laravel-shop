b19e19b39df5c8536ed7792e4a9c611da2c6d17c
zen <270281156@qq.com>
2018-11-27 08:04:16 +0000

    基础布局

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
+    public function root()
+    {
+        return view('pages.root');
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//首页
+Route::get('/','PagesController@root')->name('root');
+
+
