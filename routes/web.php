66cb78874b9786e8ad4ea850b5302dfc543caa43
zen <270281156@qq.com>
2018-11-27 10:36:59 +0000

    触发激活邮件

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,15 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
+    //手动发送验证邮箱
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
+    //邮箱验证提示
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+    //核销邮件验证码
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
 
-    Route::group(['middleware'=>'email_verified'], function () {
-        Route::get('/test', function () {
-            return "your email is verified";
-        });
+    //需要验证邮箱才可使用的路由
+    Route::group(['middleware' => 'email_verified'], function () {
+        
     });
 });
