e162c3e4e8895187817c23b168372a12defe7bb6
zen <270281156@qq.com>
2018-11-30 09:08:52 +0000

    用户界面 - 商品详情页

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@
 Route::redirect('/', '/products')->name('root');
 
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
