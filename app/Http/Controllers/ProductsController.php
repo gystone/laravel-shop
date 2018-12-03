771363ce0575a8c1d1f3278f8ef4497da5eecadf
zen <270281156@qq.com>
2018-12-03 04:48:44 +0000

    用户界面 - 收藏商品列表

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -62,13 +62,13 @@ class ProductsController extends Controller
         }
         $favored = false;
         //用户未登录时返回的是 null ，已登录时返回的是对应的用户对象
-        if($user = $request->user()){
+        if ($user = $request->user()) {
             // 从当前用户已收藏的商品中搜索 id 为当前商品 id 的商品
             // boolval() 函数用于把值转为布尔值
             $favored = boolval($user->favoriteProducts()->find($product->id));
         }
 
-        return view('products.show', ['product' => $product,'favored'=>$favored]);
+        return view('products.show', ['product' => $product, 'favored' => $favored]);
 
 
     }
@@ -93,7 +93,13 @@ class ProductsController extends Controller
         return [];
     }
 
+    //收藏列表
+    public function favorites(Request $request)
+    {
+        $products = $request->user()->favoriteProducts()->paginate(16);
+        return view('products.favorites',['products'=>$products]);
 
+    }
 
 
 }
