b97b206dd18e549f63c4b762c2cb5ef3fb1020eb
zen <270281156@qq.com>
2018-12-03 06:48:22 +0000

    购物车页面

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\AddCartRequest;
 use App\Models\CartItem;
+use App\Models\ProductSku;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
@@ -20,20 +21,29 @@ class CartController extends Controller
         if ($cart = $user->cartItems()->where('product_sku_id', $skuId)->first()) {
             // 如果存在则直接叠加商品数量
             $cart->update([
-                'amount'=>$cart->amount + $amount
+                'amount' => $cart->amount + $amount
             ]);
-        }else{
+        } else {
             // 否则创建一个新的购物车记录
             $cart = new CartItem(['amount' => $amount]);
             $cart->user()->associate($user);
             $cart->productSku()->associate($skuId);
             $cart->save();
         }
-
         return [];
+    }
 
+    public function index(Request $request)
+    {
+        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+        return view('cart.index', ['cartItems' => $cartItems]);
+    }
+
+    public function remove(ProductSku $sku,Request $request)
+    {
+        $request->user()->cartItems()->where('product_sku_id',$sku->id)->delete();
+        return [];
     }
 
 
-    
 }
