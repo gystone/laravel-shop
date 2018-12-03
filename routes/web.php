b5f1fa2c2eeaa79555674db4f74a886887b7483b
zen <270281156@qq.com>
2018-12-03 09:44:11 +0000

    创建订单

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::group(['middleware' => 'auth'], function () {
         Route::get('user_addresses/create','UserAddressesController@create')->name('user_addresses.create');
         Route::post('user_addresses','UserAddressesController@store')->name('user_addresses.store');
         Route::get('user_addresses/{user_address}','UserAddressesController@edit')->name('user_addresses.edit');
-        Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
+        Route::put('user_addresses/{user_address}', 'UserAddressesController@updat e')->name('user_addresses.update');
         Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
@@ -41,6 +41,7 @@ Route::group(['middleware' => 'auth'], function () {
         Route::post('cart', 'CartController@add')->name('cart.add');
         Route::get('cart','CartController@index')->name('cart.index');
         Route::delete('cart/{sku}','CartController@remove')->name('cart.remove');
+        Route::post('orders', 'OrdersController@store')->name('orders.store');
 
 
     });
