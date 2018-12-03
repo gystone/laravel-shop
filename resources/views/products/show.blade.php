63f62390d0bae43b3afb26edc156de27dd641342
zen <270281156@qq.com>
2018-12-03 04:00:48 +0000

    用户界面 - 商品收藏

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -15,8 +15,11 @@
                             <div class="price"><label>价格</label><em>￥</em><span>{{ $product->price }}</span></div>
                             <div class="sales_and_reviews">
                                 <div class="sold_count">累计销量 <span class="count">{{ $product->sold_count }}</span></div>
-                                <div class="review_count">累计评价 <span class="count">{{ $product->review_count }}</span></div>
-                                <div class="rating" title="评分 {{ $product->rating }}">评分 <span class="count">{{ str_repeat('★', floor($product->rating)) }}{{ str_repeat('☆', 5 - floor($product->rating)) }}</span></div>
+                                <div class="review_count">累计评价 <span class="count">{{ $product->review_count }}</span>
+                                </div>
+                                <div class="rating" title="评分 {{ $product->rating }}">评分 <span
+                                            class="count">{{ str_repeat('★', floor($product->rating)) }}{{ str_repeat('☆', 5 - floor($product->rating)) }}</span>
+                                </div>
                             </div>
                             <div class="skus">
                                 <label>选择</label>
@@ -29,22 +32,32 @@
                                                 data-toggle="tooltip"
                                                 title="{{ $sku->description }}"
                                                 data-placement="bottom">
-                                            <input type="radio" name="skus" autocomplete="off" value="{{ $sku->id }}"> {{ $sku->title }}
+                                            <input type="radio" name="skus" autocomplete="off"
+                                                   value="{{ $sku->id }}"> {{ $sku->title }}
                                         </label>
                                     @endforeach
                                 </div>
                             </div>
-                            <div class="cart_amount"><label>数量</label><input type="text" class="form-control input-sm" value="1"><span>件</span><span class="stock"></span></div>
+                            <div class="cart_amount"><label>数量</label><input type="text" class="form-control input-sm"
+                                                                             value="1"><span>件</span><span
+                                        class="stock"></span></div>
                             <div class="buttons">
-                                <button class="btn btn-success btn-favor">❤ 收藏</button>
+                                @if($favored)
+                                    <button class="btn btn-success btn-disfavor">取消收藏</button>
+                                @else
+                                    <button class="btn btn-success btn-favor">❤ 收藏</button>
+                                @endif
                                 <button class="btn btn-primary btn-add-to-cart">加入购物车</button>
                             </div>
                         </div>
                     </div>
                     <div class="product-detail">
                         <ul class="nav nav-tabs" role="tablist">
-                            <li role="presentation" class="active"><a href="#product-detail-tab" aria-controls="product-detail-tab" role="tab" data-toggle="tab">商品详情</a></li>
-                            <li role="presentation"><a href="#product-reviews-tab" aria-controls="product-reviews-tab" role="tab" data-toggle="tab">用户评价</a></li>
+                            <li role="presentation" class="active"><a href="#product-detail-tab"
+                                                                      aria-controls="product-detail-tab" role="tab"
+                                                                      data-toggle="tab">商品详情</a></li>
+                            <li role="presentation"><a href="#product-reviews-tab" aria-controls="product-reviews-tab"
+                                                       role="tab" data-toggle="tab">用户评价</a></li>
                         </ul>
                         <div class="tab-content">
                             <div role="tabpanel" class="tab-pane active" id="product-detail-tab">
@@ -68,6 +81,41 @@
                 $('.product-info .price span').text($(this).data('price'));
                 $('.product-info .stock').text('库存：' + $(this).data('stock') + '件');
             });
+
+            // 监听收藏按钮的点击事件
+            $('.btn-favor').click(function () {
+                // 发起一个 post ajax 请求，请求 url 通过后端的 route() 函数生成。
+                axios.post('{{ route('products.favor', ['product' => $product->id]) }}')
+                    .then(function () { // 请求成功会执行这个回调
+                        swal('操作成功', '', 'success')
+                            .then(function(){
+                                location.reload();
+                            });
+                    }, function (error) { // 请求失败会执行这个回调
+                        // 如果返回码是 401 代表没登录
+                        if (error.response && error.response.status === 401) {
+                            swal('请先登录', '', 'error');
+                        } else if (error.response && error.response.data.msg) {
+                            // 其他有 msg 字段的情况，将 msg 提示给用户
+                            swal(error.response.data.msg, '', 'error');
+                        } else {
+                            // 其他情况应该是系统挂了
+                            swal('系统错误', '', 'error');
+                        }
+                    });
+            });
+            //取消收藏
+            $('.btn-disfavor').click(function () {
+                axios.delete('{{ route('products.disfavor', ['product' => $product->id]) }}')
+                    .then(function () {
+                        swal('操作成功', '', 'success')
+                            .then(function () {
+                                location.reload();
+                            });
+                    });
+            });
+
+
         });
     </script>
 @endsection
