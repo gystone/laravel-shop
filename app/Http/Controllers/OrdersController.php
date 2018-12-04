210cca3390cabdaf4e383123f5b9b52dc23e2914
zen <270281156@qq.com>
2018-12-04 09:12:22 +0000

    用户界面 - 订单列表页面

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -72,6 +72,33 @@ class OrdersController extends Controller
         $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
 
         return $order;
-
     }
+
+    public function index(Request $request)
+    {
+        $orders = Order::query()
+            //使用with方法预加载，避免N+1 问题
+            ->with(['items.product', 'items.productSku'])
+            ->where('user_id', $request->user()->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate();
+
+        return view('orders.index',['orders'=>$orders]);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
