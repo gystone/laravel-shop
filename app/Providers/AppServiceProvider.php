e53ca8f7fcb025f266dc34fdd80f57a0dad9e22f
zen <270281156@qq.com>
2018-12-25 23:08:06 +0000

    订单支付

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,6 +32,9 @@ class AppServiceProvider extends ServiceProvider
         //往服务容器中注入一个名为 alipay 的单例对象
         $this->app->singleton('alipay',function () {
             $config = config('pay.alipay');
+//            $config['notify_url'] = route('payment.alipay.notify');
+            $config['notify_url'] = 'http://requestbin.fullcontact.com/130pvrp1';
+            $config['return_url'] = route('payment.alipay.return');
             //判断当前项目运行环境是否为线上环境
             if (app()->environment() != 'production') {
                 $config['model'] = 'dev';
@@ -42,6 +45,7 @@ class AppServiceProvider extends ServiceProvider
             // 调用 Yansongda\Pay 来创建一个支付宝支付对象
             return Pay::alipay($config);
         });
+
         //往服务容器中注入一个名为 wechat_pay 的单例对象
         $this->app->singleton('wechat_pay', function () {
             $config = config('pay.wechat');
