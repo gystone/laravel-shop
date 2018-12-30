e1905fc07325074f75f619c68283dad1a96e64c7
zen <270281156@qq.com>
2018-12-30 02:21:49 +0000

    用户界面 - 申请退款

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -28,23 +28,36 @@
                                     </div>
                                     <div>
             <span class="product-title">
-               <a target="_blank" href="{{ route('products.show', [$item->product_id]) }}">{{ $item->product->title }}</a>
+               <a target="_blank"
+                  href="{{ route('products.show', [$item->product_id]) }}">{{ $item->product->title }}</a>
              </span>
                                         <span class="sku-title">{{ $item->productSku->title }}</span>
                                     </div>
                                 </td>
                                 <td class="sku-price text-center vertical-middle">￥{{ $item->price }}</td>
                                 <td class="sku-amount text-center vertical-middle">{{ $item->amount }}</td>
-                                <td class="item-amount text-right vertical-middle">￥{{ number_format($item->price * $item->amount, 2, '.', '') }}</td>
+                                <td class="item-amount text-right vertical-middle">
+                                    ￥{{ number_format($item->price * $item->amount, 2, '.', '') }}</td>
                             </tr>
                         @endforeach
-                        <tr><td colspan="4"></td></tr>
+                        <tr>
+                            <td colspan="4"></td>
+                        </tr>
                     </table>
                     <div class="order-bottom">
                         <div class="order-info">
-                            <div class="line"><div class="line-label">收货地址：</div><div class="line-value">{{ join(' ', $order->address) }}</div></div>
-                            <div class="line"><div class="line-label">订单备注：</div><div class="line-value">{{ $order->remark ?: '-' }}</div></div>
-                            <div class="line"><div class="line-label">订单编号：</div><div class="line-value">{{ $order->no }}</div></div>
+                            <div class="line">
+                                <div class="line-label">收货地址：</div>
+                                <div class="line-value">{{ join(' ', $order->address) }}</div>
+                            </div>
+                            <div class="line">
+                                <div class="line-label">订单备注：</div>
+                                <div class="line-value">{{ $order->remark ?: '-' }}</div>
+                            </div>
+                            <div class="line">
+                                <div class="line-label">订单编号：</div>
+                                <div class="line-value">{{ $order->no }}</div>
+                            </div>
                             <!-- 输出物流状态 -->
                             <div class="line">
                                 <div class="line-label">物流状态：</div>
@@ -55,6 +68,17 @@
                                 <div class="line">
                                     <div class="line-label">物流信息：</div>
                                     <div class="line-value">{{ $order->ship_data['express_company'] }} {{ $order->ship_data['express_no'] }}</div>
+                                </div>
+                            @endif
+                        <!-- 订单已支付，且退款状态不是未退款时展示退款信息 -->
+                            @if($order->paid_at && $order->refund_status !== \App\Models\Order::REFUND_STATUS_PENDING)
+                                <div class="line">
+                                    <div class="line-label">退款状态：</div>
+                                    <div class="line-value">{{ \App\Models\Order::$refundStatusMap[$order->refund_status] }}</div>
+                                </div>
+                                <div class="line">
+                                    <div class="line-label">退款理由：</div>
+                                    <div class="line-value">{{ $order->extra['refund_reason'] }}</div>
                                 </div>
                             @endif
                         </div>
@@ -82,18 +106,26 @@
                                 @if($order->ship_status === \App\Models\Order::SHIP_STATUS_DELIVERED)
                                     <div class="receive-button">
                                         <!-- 将原本的表单替换成下面这个按钮 -->
-                                        <button type="button" id="btn-receive" class="btn btn-sm btn-success">确认收货</button>
+                                        <button type="button" id="btn-receive" class="btn btn-sm btn-success">确认收货
+                                        </button>
 
+                                    </div>
+                                @endif
+                            <!-- 订单已支付，且退款状态是未退款时展示申请退款按钮 -->
+                                @if($order->paid_at && $order->refund_status === \App\Models\Order::REFUND_STATUS_PENDING)
+                                    <div class="refund-button">
+                                        <button class="btn btn-sm btn-danger" id="btn-apply-refund">申请退款</button>
                                     </div>
                                 @endif
                             </div>
                             <!--支付按钮开始-->
                             @if(!$order->paid_at && !$order->closed)
                                 <div class="payment-buttons">
-                                    <a class="btn btn-primary btn-sm" href="{{ route('payment.alipay', ['order' => $order->id]) }}">支付宝支付</a>
+                                    <a class="btn btn-primary btn-sm"
+                                       href="{{ route('payment.alipay', ['order' => $order->id]) }}">支付宝支付</a>
                                     <a class="btn btn-success btn-sm" href="#">微信支付</a>
                                 </div>
-                            @endif
+                        @endif
                         <!-- 支付按钮结束 -->
                         </div>
                     </div>
@@ -106,9 +138,9 @@
 
 @section('scriptsAfterJs')
     <script>
-        $(document).ready(function() {
+        $(document).ready(function () {
             // 确认收货按钮点击事件
-            $('#btn-receive').click(function() {
+            $('#btn-receive').click(function () {
                 // 弹出确认框
                 swal({
                     title: "确认已经收到商品？",
@@ -117,7 +149,7 @@
                     dangerMode: true,
                     buttons: ['取消', '确认收到'],
                 })
-                    .then(function(ret) {
+                    .then(function (ret) {
                         // 如果点击取消按钮则不做任何操作
                         if (!ret) {
                             return;
@@ -130,6 +162,30 @@
                             })
                     });
             });
+            // 退款按钮点击事件
+            $('#btn-apply-refund').click(function () {
+                swal({
+                    text: '请输入退款理由',
+                    content: "input",
+                }).then(function (input) {
+                    // 当用户点击 swal 弹出框上的按钮时触发这个函数
+                    if (!input) {
+                        swal('退款理由不可空', '', 'error');
+                        return;
+                    }
+                    // 请求退款接口
+                    axios.post('{{ route('orders.apply_refund', [$order->id]) }}', {reason: input})
+                        .then(function () {
+                            swal('申请退款成功', '', 'success').then(function () {
+                                // 用户点击弹框上按钮时重新加载页面
+                                location.reload();
+                            });
+                        }).catch(function (error) {
+                        swal(error.response.data.msg, '', 'error');
+                    });
+                });
+            });
+
 
         });
     </script>
